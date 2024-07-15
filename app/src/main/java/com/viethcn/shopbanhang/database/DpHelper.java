b00329e49aa3d72cdd108b66b0930169c8d253ac
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-07-15 15:13:13 +0700

    oki

---
+++ app/src/main/java/com/viethcn/shopbanhang/database/DpHelper.java
@@ -0,0 +1,21 @@
+package com.viethcn.shopbanhang.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DpHelper extends SQLiteOpenHelper {
+    public DpHelper(Context context) {
+        super(context,"banhang",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
