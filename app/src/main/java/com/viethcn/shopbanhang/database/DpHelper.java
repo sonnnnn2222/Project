262b2f113f7798918f5b44f4066d5521eec50803
PhuQuy <156397083+Crislk1010@users.noreply.github.com>
2024-07-16 10:26:49 +0700

    Update DpHelper.java

--- app/src/main/java/com/viethcn/shopbanhang/database/DpHelper.java
+++ app/src/main/java/com/viethcn/shopbanhang/database/DpHelper.java
@@ -11,6 +11,7 @@ public class DpHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //THUTHU(matt text, hoten text, matkhau text)
         String dbThuThu = "create table THUTHU(matt text primary key, hoten text, matkhau text)";
         db.execSQL(dbThuThu);
     }
