a320258cd35eaafc45214e0b5af555bded17788d
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-07-16 14:40:36 +0700

    dbhelper

--- app/src/main/java/com/viethcn/shopbanhang/database/DpHelper.java
+++ app/src/main/java/com/viethcn/shopbanhang/database/DpHelper.java
@@ -16,13 +16,19 @@ public class DpHelper extends SQLiteOpenHelper {
         //THUTHU(matt text, hoten text, matkhau text)
         String dbThuThu = "create table THUTHU(matt text primary key, hoten text, matkhau text)";
         db.execSQL(dbThuThu) ;
+
         String dbThanhVien = "create table THANHVIEN(matv integer primary key autoincrement, hoten text, namsinh text )";
         db.execSQL(dbThanhVien);
 
+        String dbLoaiSach = "create table LOAISACH(maloai integer primary key autoincrement, tenloai text)";
+        db.execSQL(dbLoaiSach);
 
+        String dbSach = "create table SACH(masach integer primary key autoincrement, tensach text, giathue integer, maloai integer references LOAISACH(maloai))";
+        db.execSQL(dbSach);
 
-   
-
+        String dbPhieuMuon = "create table PHIEUMUON(maphieumuon integer primary key autoincrement, matv integer references THANHVIEN(matv), matt text references THUTHU(matt), masach integer references SACH(masach), ngay text, trasach integer, tienthue integer)";
+        db.execSQL(dbPhieuMuon);
+        
     }
 
     @Override
