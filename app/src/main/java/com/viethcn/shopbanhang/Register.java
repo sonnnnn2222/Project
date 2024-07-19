0bf68312f2cb4599e61905e567287790c9f81a9e
TranGiaBaoPS41156 <trangiabao22172005@gmail.com>
2024-07-19 14:16:57 +0700

    Update Register.java

--- app/src/main/java/com/viethcn/shopbanhang/Register.java
+++ app/src/main/java/com/viethcn/shopbanhang/Register.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.viethcn.shopbanhang.dao.NguoiDungDAO;
 
+
 public class Register extends AppCompatActivity {
     private NguoiDungDAO nguoiDungDAO;
     @Override
