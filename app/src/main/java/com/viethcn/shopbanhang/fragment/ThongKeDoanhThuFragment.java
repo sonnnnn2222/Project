cf6d0f935f840ec5991339571f0d564dd71f6206
crislk <nhocquy2012@gmail.com>
2024-07-22 22:47:12 +0700

    add ThongKeDoanhThu

--- app/src/main/java/com/viethcn/shopbanhang/fragment/ThongKeDoanhThuFragment.java
+++ app/src/main/java/com/viethcn/shopbanhang/fragment/ThongKeDoanhThuFragment.java
@@ -1,10 +1,61 @@
 package com.viethcn.shopbanhang.fragment;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.viethcn.shopbanhang.database.DbHelper;
+import com.viethcn.shopbanhang.R;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 public class ThongKeDoanhThuFragment extends Fragment {
-    DbHelper db;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_thongkedoanhthu, container, false);
 
+        EditText edtStart = view.findViewById(R.id.edtStart);
+        EditText edtEnd = view.findViewById(R.id.edtEnd);
+        Button btnThongKe = view.findViewById(R.id.btnThongKe);
+        TextView txtDoanhThu = view.findViewById(R.id.txtDoanhThu);
+
+        Calendar calendar = Calendar.getInstance();
+
+        edtStart.setOnClickListener( v ->{
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    getContext(),
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                            String ngay = dayOfMonth < 10? ("0" + dayOfMonth) : String.valueOf(dayOfMonth);
+                            String thang = month < 10? ("0" + month) : String.valueOf(month);
+                            edtStart.setText(year + "/" + month + "/" + dayOfMonth);
+                        }
+                    },
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            );
+            datePickerDialog.show();
+        });
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
