36fd78885978f35c11ac75b8f8c289a0594ddbba
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-07-23 22:20:55 +0700

    sua cau truy van

--- app/src/main/java/com/viethcn/shopbanhang/dao/PhieuMuonDAO.java
+++ app/src/main/java/com/viethcn/shopbanhang/dao/PhieuMuonDAO.java
@@ -22,7 +22,7 @@ public class PhieuMuonDAO {
     public ArrayList<PhieuMuon> getDSPhieuMuon() {
         ArrayList<PhieuMuon> list = new ArrayList<>();
         SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT pm.mapm,  pm.matv, tv.hoten , pm.tendangnhap,  nd.hoten ,  pm.masach,  sc.tensach, pm.ngay,  pm.trasach,  pm.tienthue FROM PHIEUMUON pm JOIN THANHVIEN tv ON pm.matv = tv.matv JOIN NGUOIDUNG nd ON pm.tendangnhap = nd.tendangnhap JOIN  SACH sc ON pm.masach = sc.masach", null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT pm.mapm,  pm.matv, tv.hoten , pm.tendangnhap,  nd.hoten ,  pm.masach,  sc.tensach, pm.ngay,  pm.trasach,  pm.tienthue FROM PHIEUMUON pm JOIN THANHVIEN tv ON pm.matv = tv.matv JOIN NGUOIDUNG nd ON pm.tendangnhap = nd.tendangnhap JOIN  SACH sc ON pm.masach = sc.masach order by pm.mapm desc", null);
         if (cursor.getCount() != 0 ) {
             cursor.moveToFirst();
             do {
