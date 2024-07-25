bfed5774394ea95323240e41581f94c518723144
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-07-25 23:35:47 +0700

    top10 muon sach nhieu nhat

--- app/src/main/java/com/viethcn/shopbanhang/dao/ThongKeDAO.java
+++ app/src/main/java/com/viethcn/shopbanhang/dao/ThongKeDAO.java
@@ -5,11 +5,27 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.viethcn.shopbanhang.database.DbHelper;
+import com.viethcn.shopbanhang.model.Sach;
+
+import java.util.ArrayList;
 
 public class ThongKeDAO {
     DbHelper dbHelper;
     public ThongKeDAO(Context c) {
         dbHelper = new DbHelper(c);
+    }
+
+    public ArrayList<Sach> getTop10() {
+        ArrayList<Sach> list = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT pm.masach, sc.tensach, COUNT(pm.masach) from PHIEUMUON pm, SACH sc WHERE pm.masach = sc.masach GROUP by pm.masach, sc.tensach ORDER by COUNT(pm.masach) DESC limit 10", null);
+        if (cursor.getCount() != 0) {
+            cursor.moveToFirst();
+            do {
+                list.add(new Sach(cursor.getInt(0), cursor.getString(1), cursor.getInt(2)));
+            }while (cursor.moveToNext());
+        }
+        return list;
     }
 
     public int getDoanhThu(String start, String end){
--- app/src/main/java/com/viethcn/shopbanhang/model/Sach.java
+++ app/src/main/java/com/viethcn/shopbanhang/model/Sach.java
@@ -6,6 +6,7 @@ public class Sach {
     private int giathue;
     private int maloai;
     private String tenloai;
+    private int soLuongDaMuon;
 
     public Sach(int masach, String tenSach, int giathue, int maloai, String tenloai) {
         this.masach = masach;
@@ -15,6 +16,19 @@ public class Sach {
         this.tenloai = tenloai;
     }
 
+    public Sach(int masach, String tenSach, int soLuongDaMuon) {
+        this.masach = masach;
+        this.tenSach = tenSach;
+        this.soLuongDaMuon = soLuongDaMuon;
+    }
+
+    public int getSoLuongDaMuon() {
+        return soLuongDaMuon;
+    }
+
+    public void setSoLuongDaMuon(int soLuongDaMuon) {
+        this.soLuongDaMuon = soLuongDaMuon;
+    }
 
     public int getMasach() {
         return masach;
