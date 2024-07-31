604a33f304c0117deefd62826e069fb7ef2cfe08
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-07-31 11:31:02 +0700

    man hinh chao

--- app/src/main/java/com/viethcn/shopbanhang/WelcomeActivity.java
+++ app/src/main/java/com/viethcn/shopbanhang/WelcomeActivity.java
@@ -14,6 +14,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
@@ -22,29 +24,8 @@ public class WelcomeActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
         ImageView bookCover = findViewById(R.id.bookCover);
-        TextView appTitle = findViewById(R.id.appTitle);
-        TextView appSubtitle = findViewById(R.id.appSubtitle);
+        Glide.with(this).load(R.drawable.schoolbook).into(bookCover);
 
-        // Load animations
-        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        Animation slideUp = AnimationUtils.loadAnimation(this, R.anim.slide_up);
 
-        // Apply animations
-        bookCover.startAnimation(fadeIn);
-        appTitle.startAnimation(slideUp);
-        appSubtitle.startAnimation(slideUp);
-        new CountDownTimer(3000,3000){
-
-            @Override
-            public void onTick(long millisUntilFinished) {
-
-            }
-
-            @Override
-            public void onFinish() {
-                Intent intent = new Intent(WelcomeActivity.this,Login.class);
-                startActivity(intent);
-            }
-        }.start();
     }
-    }
+}
