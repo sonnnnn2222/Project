49cfcd8e627d6d0454a2736f461275ea07b7b775
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-08-02 15:24:58 +0700

    sua form thong tin

--- app/src/main/java/com/viethcn/shopbanhang/fragment/ThongTinFragment.java
+++ app/src/main/java/com/viethcn/shopbanhang/fragment/ThongTinFragment.java
@@ -6,37 +6,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.viethcn.shopbanhang.R;
-import com.viethcn.shopbanhang.adapter.LoaiSachAdapter;
-import com.viethcn.shopbanhang.dao.LoaiSachDAO;
-import com.viethcn.shopbanhang.model.ItemClick;
-import com.viethcn.shopbanhang.model.LoaiSach;
-
-import java.util.ArrayList;
 
 public class ThongTinFragment extends Fragment {
 
 
-    private TextView txtDangNhap, txtHoTen, txtMatKhau;;
+    private EditText txtDangNhap, txtHoTen, txtMatKhau;;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.thongtin_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_thongtin, container, false);
         txtDangNhap  = view.findViewById(R.id.txtDangNhap);
         txtHoTen  = view.findViewById(R.id.txtHoTen);
         txtMatKhau  = view.findViewById(R.id.txtMatKhau);
+        txtMatKhau.setEnabled(false);
+        txtHoTen.setEnabled(false);
+        txtDangNhap.setEnabled(false);
 
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("thongtin", Context.MODE_PRIVATE);
         String dangNhap = sharedPreferences.getString("tendangnhap", "");
