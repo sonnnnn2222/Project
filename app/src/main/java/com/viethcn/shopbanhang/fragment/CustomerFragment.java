d2197e21506b4af772eb522c4ede204504299c3f
crislk <nhocquy2012@gmail.com>
2024-08-06 02:00:31 +0700

    changing some setting for UI

--- app/src/main/java/com/viethcn/shopbanhang/fragment/CustomerFragment.java
+++ app/src/main/java/com/viethcn/shopbanhang/fragment/CustomerFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +11,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.viethcn.shopbanhang.R;
 import com.viethcn.shopbanhang.adapter.CategoryAdapter;
 import com.viethcn.shopbanhang.adapter.MainViewCustomerAdapter;
@@ -26,7 +27,7 @@ public class CustomerFragment extends Fragment {
     private RecyclerView rcvCategory, rcvBook;
     private LoaiSachDAO CategoryDAO;
     private SachDAO bookDAO;
-    private List<String> listCategory, listBook;
+    private ImageSlider imgSlider;
 
     @Nullable
     @Override
@@ -38,16 +39,19 @@ public class CustomerFragment extends Fragment {
 
         rcvCategory = view.findViewById(R.id.rcvCategory);
         rcvBook = view.findViewById(R.id.rcvBook);
+        imgSlider = view.findViewById(R.id.imgSlider);
 
         showCategory();
         showBook();
+        setSlider();
+
 
         return view;
     }
 
 
     private void showCategory() {
-        listCategory = new ArrayList<>();
+        List<String> listCategory = new ArrayList<>();
         listCategory = CategoryDAO.getCategoryName();
         CategoryAdapter adapter = new CategoryAdapter(listCategory);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
@@ -56,11 +60,19 @@ public class CustomerFragment extends Fragment {
     }
 
     private void showBook(){
-        listBook = new ArrayList<>();
-        listBook = bookDAO.getBookName();
+        List<String> listBook = bookDAO.getBookName();
         MainViewCustomerAdapter adapter = new MainViewCustomerAdapter(getContext(), listBook);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         rcvBook.setLayoutManager(layoutManager);
         rcvBook.setAdapter(adapter);
     }
+
+    private void setSlider() {
+        List<SlideModel> listImg = new ArrayList<>();
+        listImg.add(new SlideModel(R.drawable.book_rent_banner , ScaleTypes.FIT));
+        listImg.add(new SlideModel(R.drawable.book_rent ,ScaleTypes.FIT));
+
+        imgSlider.setImageList(listImg);
+    }
+
 }
